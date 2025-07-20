ea472d6951658a3c94ffa33d0c210dae06852179
yujinnyujin <0rangesunset@naver.com>
2025-07-20 20:07:57 +0900

    Object 다형성

---
+++ java-mid1/src/lang/object/poly/ObjectPolyExample1.java
@@ -0,0 +1,26 @@
+package lang.object.poly;
+
+public class ObjectPolyExample1 {
+
+    public static void main(String[] args) {
+        Dog dog = new Dog();
+        Car car = new Car();
+
+        action(dog);
+        action(car);
+    }
+
+    private static void action(Object obj){
+        //obj.sound(); //컴파일 오류, Object는 sound()가 없다
+        //obj.move(); //컴파일 오류, Object는 move()가 없다
+
+        //객체에 맞는 다운캐스팅 필요
+        if(obj instanceof Dog){
+            ((Dog)obj).sound();
+        } else if(obj instanceof Car){
+            ((Car)obj).move();
+        }
+    }
+
+
+}
